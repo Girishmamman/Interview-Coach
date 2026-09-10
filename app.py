e4ab1fcd80dd8f09e979d89076b30befdd8c99f7
girish <mammangirish@gmail.com>
2026-09-10 11:08:09 +0530

    signup functionality for new users with flask route

--- app.py
+++ app.py
@@ -60,4 +60,52 @@ def login():
 
     return render_template('login.html')
 
+# SIGNUP PAGE
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+
+    if request.method == 'POST':
+
+        username = request.form['username']
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
+
+        if password != confirm_password:
+            return render_template(
+                "signup.html",
+                error="Passwords do not match"
+            )
+
+        conn = sqlite3.connect(DB_PATH)
+        cur = conn.cursor()
+
+        cur.execute(
+            "SELECT * FROM users WHERE username=?",
+            (username,)
+        )
+
+        user = cur.fetchone()
+
+        if user:
+            conn.close()
+            return render_template(
+                "signup.html",
+                error="Username already exists"
+            )
+
+        cur.execute(
+            "INSERT INTO users(username, password) VALUES (?, ?)",
+            (username, password)
+        )
+
+        conn.commit()
+        conn.close()
+
+        session['username'] = username
+
+        return redirect(url_for('dashboard'))
+
+    return render_template("signup.html")
+
 
